a28f4ad21b392bf5ff65491693febf02f67b11a0
Rachael Mueller <rmueller@eoas.ubc.ca>
2019-03-22 15:10:36 -0700

    Adjusted Shihan's .m script to use AfterNEMOBathy201702.nc bathy

--- toolbox/matlab/MOHID_nc2dat_bathymetry.m
+++ toolbox/matlab/MOHID_nc2dat_bathymetry.m
@@ -1,34 +1,32 @@
 
 clear all; close all;
-%%%%%%%set the time steps need to be converted
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-file='I:\Vancouver 2016\vancouver Harbour MOHID\nemo data\bathymetry.nc'; %%%Bathymetry file
+%%%%%%%set the time steps need to be converted %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+coords_file = '/home/rmueller/Data/SalishSeaCast/grid/bathymetry_201702.nc'; %% lat & lon values
+bathy_file  = '/home/rmueller/Data/SalishSeaCast/grid/AfterNEMOBathy201702.nc'; %% bathymetry
+out_file    = '/home/rmueller/Data/SalishSeaCast/grid/AfterNEMOBathy201702_rdm.dat'; %% output file
 
-lon1=double(ncread(file,'longitude'));%%% grid center lon lat
-lat1=double(ncread(file,'latitude'));
- 
+lon1=double(ncread(coords_file,'nav_lon'));%%% grid center lon lat
+lat1=double(ncread(coords_file,'nav_lat'));
+
+%RDM note: [M,N] = [aa,bb] =  size(lon1)   
 aa=398;bb=898;cc=40;  %%%% NEMO grid dimension
+display(['NEMO dimensions: [' num2str(aa) ',' num2str(bb) ']']);
 
-aaa=num2str(aa-2,'%03d'); 
+aaa=num2str(aa-2,'%03d'); % interior points
 bbb=num2str(bb-2,'%03d');
 ccc=num2str(cc,'%03d');
 
 %%%% set up grid for MOHID (MOHID grid dimensions are 1 longer than its data)
+%% RDM: Average lat/lon to f-points...?
 for x=1:aa-1;
     for y=1:bb-1;
         grid_lon(x,y)=(lon1(x,y)+lon1(x,y+1)+lon1(x+1,y+1)+lon1(x+1,y))/4;
         grid_lat(x,y)=(lat1(x,y)+lat1(x,y+1)+lat1(x+1,y+1)+lat1(x+1,y))/4;
     end
 end
+[sgy,sgx] = size(grid_lon);
+display(['grid_lon dimensions: [' num2str(sgy) ',' num2str(sgx) ']']);
  
-%%%% new grid center lon lat in MOHID (one line of data are abandoned at 
-%%%% right, left, top, and bottom respectively, due to the different grid in MOHID)
-data_lon(1:aa-2,1:bb-2)=lon1(2:aa-1,2:bb-1);
-data_lat(1:aa-2,1:bb-2)=lat1(2:aa-1,2:bb-1);
-
-da_lon=permute(data_lon,[2,1]);
-da_lat=permute(data_lat,[2,1]);
-
 %%%% MOHID and NEMO have different directions for data
 chunk_size_2D=[bb-2,aa-2];
 chunk_size_3D=[bb-2,aa-2,cc];
@@ -36,7 +34,7 @@ start_lon=num2str(grid_lon(1,1),'%8f');
 start_lat=num2str(grid_lat(1,1),'%8f');
 %%
 %%%%%%%%%%create the grid and bathymetry file for MOHID
-fid=fopen('I:\Vancouver 2016\vancouver Harbour MOHID\map\data\ST_georgia_bathymetry_revisit.dat','wt');
+fid=fopen(out_file, 'wt');
 fprintf(fid,'%s\n','COORD_TIP     : 4');
 fprintf(fid,'%s\n',['ILB_IUB       : 1 ',bbb]);
 fprintf(fid,'%s\n',['JLB_JUB       : 1 ',aaa]);
@@ -61,8 +59,9 @@ end;
 fprintf(fid,'%s\n','<\CornersXY>');
 fprintf(fid,'%s\n','');
 %%%%%%%%%%%%%%%new%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-depth=ncread(file,'bathymetry',[2,2],[396,896]);
-
+depth=ncread(bathy_file,'Bathymetry',[2,2],[396,896]);
+[sgy,sgx] = size(depth);
+display(['depth dimensions: [' num2str(sgy) ',' num2str(sgx) ']']);
 
 %idz=find(depth==0);
 idq=find(isnan(depth));
