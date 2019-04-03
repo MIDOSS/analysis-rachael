4ea2f60bd5b9faa96c21984f762cc09320209ce5
Rachael Mueller <rmueller@eoas.ubc.ca>
2019-04-03 08:55:21 -0700

    Adding Shihan's script to create a masked e3t hdf5 file

---
+++ matlab/convert_e3t_to_hdf5_masked.m
@@ -0,0 +1,99 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This program will generate a new e3t.hdf5 file from NEMO output
+% from /results2/SalishSea/nowcast-green.201806/
+%
+% This version corrects a problem created by differences in land mask between NEMO and
+% MOHID by....
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; close all;
+%%%%%%% set the time steps need to be converted
+time=168;
+
+aa=398;bb=898;cc=40;  %%%% NEMO grid dimension
+
+aaa=num2str(aa-2,'%03d');
+bbb=num2str(bb-2,'%03d');
+ccc=num2str(cc,'%03d');
+
+%%%%%%%%%%%%%create hdf5 current files
+dataname=('I:\MOHID test data\hdf\e3t_3.hdf5');
+datao=('I:\UBC file\e3t.hdf5');
+datat=('I:\UBC file\\t.hdf5');
+fid = H5F.create(dataname);
+plist = 'H5P_DEFAULT';
+
+%%%%%%%%%%%%create groups in hdf5 based on the MOHID format
+gid = H5G.create(fid,'/Results',plist,plist,plist);
+H5G.close(gid);
+gid = H5G.create(fid,'/Results/e3t',plist,plist,plist);
+
+H5G.close(gid);
+gid = H5G.create(fid,'/Time',plist,plist,plist);
+H5G.close(gid);
+H5F.close(fid);
+
+%%%%%%%%%%%%%%write group attitude
+fid=(dataname);
+
+h5writeatt(fid,'/Results','Minimum',0);
+h5writeatt(fid,'/Results','Maximum',200);
+h5writeatt(fid,'/Results/e3t','Minimum',0);
+h5writeatt(fid,'/Results/e3t','Maximum',200);
+
+h5writeatt(fid,'/Time','Minimum',-0.000000);
+h5writeatt(fid,'/Time','Maximum',2016.000000);
+disp('group created')
+
+
+file='I:\Vancouver 2016\vancouver Harbour MOHID\nemo data\bathymetry.nc'; %%%Bathymetry file
+file1='I:\Vancouver 2016\vancouver Harbour MOHID\nemo data\u_07.nc'; %%%Current U
+file2='I:\Vancouver 2016\vancouver Harbour MOHID\nemo data\v_07.nc'; %%%Current V
+file3='I:\Vancouver 2016\vancouver Harbour MOHID\nemo data\grid.nc';
+file11='C:\work\data sample\SoG Currents\WC3_CU60_20020101_20020110_grid_U.nc';
+lon1=double(ncread(file,'longitude'));%%% grid center lon lat
+lat1=double(ncread(file,'latitude'));
+ 
+
+% %%%% MOHID and NEMO have different directions for data
+chunk_size_2D=[bb-2,aa-2];
+chunk_size_3D=[bb-2,aa-2,cc];
+% start_lon=num2str(grid_lon(1,1),'%8f');
+% start_lat=num2str(grid_lat(1,1),'%8f');
+%%
+
+
+for t=1:time;
+  disp(t);
+    time_counter=num2str(t,'%05d');
+    dirt=['/Time/Time_',time_counter];
+    date=h5read(datao,dirt);
+    directory=['/Time/Time_',time_counter];
+    h5create(fid,directory,6,'ChunkSize',6,'Deflate',6);
+    h5writeatt(fid,directory,'Minimum',-0.000000);
+    h5writeatt(fid,directory,'Maximum',2020.000000);
+    h5writeatt(fid,directory,'Units','YYYY/MM/DD HH:MM:SS');
+    h5write(fid,directory,date);
+
+%%%%%%%%%%%%loop for write uv by time
+
+    diru=['/Results/e3t/e3t_',time_counter];
+    dirs=['/Results/salinity/salinity_',time_counter];
+    sal=h5read(datat,dirs);
+    ipx=find(sal~=0);
+    sal(ipx)=1;
+    mask3d=sal;
+    e3t1=h5read(datao,diru);
+    e3t=e3t1.*mask3d;
+
+%%%%%%%%%%%%%%%%% apply u v at cell center
+
+   directory=['/Results/e3t/e3t_',time_counter];
+   h5create(fid,directory,chunk_size_3D,'ChunkSize',chunk_size_3D,'Deflate',6);
+   h5writeatt(fid,directory,'Minimum',0);
+   h5writeatt(fid,directory,'Maximum',200);
+   h5writeatt(fid,directory,'Units','m');
+   h5writeatt(fid,directory,'FillValue',0);
+   h5write(fid,directory,e3t);
+ 
+end
