1e63ade7b6210e281f7b6ca6db9a3824e23612cb
Rachael D. Mueller <rmueller@eoas.ubc.ca>
2020-07-07 13:43:20 -0700

    Added error checks and annotations The error checks are for cases where there is no oil transfer for specified location and ship type In these cases, I identify the ship track as a fuel spill hazard and not a cargo hazard I also identify as fuel hazard the barges with orign = Pacific, US or Canada The code has detailed comments that describe the motivation for these choices

--- notebooks/monte_carlo/get_oil_type_barge.py
+++ notebooks/monte_carlo/get_oil_type_barge.py
@@ -4,7 +4,9 @@ import pathlib
 from monte_carlo_utils import get_oil_type_cargo 
 
 #### Decision tree for allocating oil type to barge traffic
-# see google drawing [Barge_Oil_Attribution](https://docs.google.com/drawings/d/10PM53-UnnILYCAPKU9MxiR-Y4OW0tIMhVzSjaHr-iSc/edit) for a visual representation
+# see google drawing 
+# [Barge_Oil_Attribution](https://docs.google.com/drawings/d/10PM53-UnnILYCAPKU9MxiR-Y4OW0tIMhVzSjaHr-iSc/edit)
+# for a visual representation
 
 def get_oil_type_barge(master_dir,
                     master_file,
@@ -20,9 +22,9 @@ def get_oil_type_barge(master_dir,
     # instead of cargo-spill risk
     fuel_flag = 0
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##  Load file paths and terminal names
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     with open(f'{master_dir}{master_file}') as file:
         master = yaml.safe_load(file)
 
@@ -42,51 +44,158 @@ def get_oil_type_barge(master_dir,
     Pacific_yaml = home/master['files']['Pacific_origin']
 
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # these pairs need to be used together for "get_oil_type_cargo" (but don't yet have error-checks in place):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # these pairs need to be used together for "get_oil_type_cargo" 
+    # (but don't yet have error-checks in place):
     # - "WA_in_yaml" and "destination"
     # - "WA_out_yaml" and "origin"
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    if origin in CAD_origin_destination:
+    #
+    # ERROR CATCH for case of no fuel transfer for given selection of 
+    # yaml file, origin, and ship_type is currently to set flag to fuel
+    # spill potential and not cargo spill potential
+    #
+    # Why? 
+    #
+    # Because there are lots of tugs that are not associated
+    # with oil tank barges.  The way Casey sub-sampled the AIS data for 
+    # tank barges was to select all barges that docked at a marine
+    # terminal; however, it's possible that the tugs docked at these
+    # marine terminals to fuel up instead of tank-up. This traffic may 
+    # appear in cases where origin/destination is an oil terminal but 
+    # where no oil cargo is transferred.  In these cases, the spill 
+    # potential is deemed as a fuel spill potential and fuel_flag is 
+    # set to 1.   
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   
+    if origin in CAD_origin_destination:        
         if origin == 'Westridge Marine Terminal':
             if destination in CAD_origin_destination:
-                fuel_type = 'jet'
+                fuel_type = 'jet'            
             else:
-                fuel_type = get_oil_type_cargo(CAD_yaml, origin, ship_type, random_seed)
+                fuel_type = get_oil_type_cargo( 
+                          CAD_yaml, origin, ship_type, random_seed)
+                
+                # *** ERROR CATCH ***
+                if not fuel_type:
+                    fuel_flag = 1
+                    fuel_type = []
+                # *** END ERROR CATCH ***   
         else:
             if destination in US_origin_destination:
-                # we have better information on WA fuel transfers, so I'm prioritizing this information source
-                fuel_type = get_oil_type_cargo(WA_in_yaml, destination, ship_type, random_seed)
+                # we have better information on WA fuel transfers, 
+                # so I'm prioritizing this information source
+                fuel_type = get_oil_type_cargo(
+                          WA_in_yaml, destination, 
+                          ship_type, random_seed)
+                
+                # *** ERROR CATCH ***
+                if not fuel_type:
+                    fuel_flag = 1
+                    fuel_type = []
+                # *** END ERROR CATCH *** 
+                
             elif destination == 'ESSO Nanaimo Departure Bay':
-                fuel_type = get_oil_type_cargo(CAD_yaml, destination, ship_type, random_seed)
+                fuel_type = get_oil_type_cargo(
+                          CAD_yaml, destination, 
+                          ship_type, random_seed)
+                
+                # *** ERROR CATCH ***
+                if not fuel_type:
+                    fuel_flag = 1
+                    fuel_type = []
+                # *** END ERROR CATCH ***  
+                
             elif destination == 'Suncor Nanaimo':
-                fuel_type = get_oil_type_cargo(CAD_yaml, destination, ship_type, random_seed)
+                fuel_type = get_oil_type_cargo(
+                          CAD_yaml, destination, 
+                          ship_type, random_seed)
+                
+                # *** ERROR CATCH ***
+                if not fuel_type:
+                    fuel_flag = 1
+                    fuel_type = []
+                # *** END ERROR CATCH *** 
+                
             else: 
-                fuel_type = get_oil_type_cargo(CAD_yaml, origin, ship_type, random_seed)
+                fuel_type = get_oil_type_cargo(
+                          CAD_yaml, origin, ship_type, random_seed)
+                
+                # *** ERROR CATCH ***
+                if not fuel_type:
+                    fuel_flag = 1
+                    fuel_type = []
+                # *** END ERROR CATCH ***  
+                
     elif origin in US_origin_destination:
-        fuel_type = get_oil_type_cargo(WA_out_yaml, origin, ship_type, random_seed)
+        fuel_type = get_oil_type_cargo(
+                  WA_out_yaml, origin, ship_type, random_seed)
+        
+        # *** ERROR CATCH ***
+        if not fuel_type:
+            fuel_flag = 1
+            fuel_type = []
+        # *** END ERROR CATCH *** 
+
     elif destination in US_origin_destination:
-        fuel_type = get_oil_type_cargo(WA_in_yaml, destination, ship_type, random_seed)
+        fuel_type = get_oil_type_cargo(
+                  WA_in_yaml, destination, ship_type, random_seed)
+        
+        # *** ERROR CATCH ***
+        if not fuel_type:
+            fuel_flag = 1
+            fuel_type = []
+        # *** END ERROR CATCH *** 
+        
     elif destination in CAD_origin_destination:
         if destination == 'Westridge Marine Terminal':
             # Westridge doesn't receive crude for storage 
             fuel_type = 'jet'
         else:
-            fuel_type = get_oil_type_cargo(CAD_yaml, destination, ship_type, random_seed)
-    # All remaining cases are deemed non-tank traffic
+            fuel_type = get_oil_type_cargo(
+                      CAD_yaml, destination, ship_type, random_seed)
+            
+            # *** ERROR CATCH ***
+            if not fuel_type:
+                fuel_flag = 1
+                fuel_type = []
+            # *** END ERROR CATCH *** 
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # All remaining cases are deemed non-tank traffic. Cam and I tried
+    # to eliminate as much non-oil-cargo barges as possible by 
+    # evaluating each terminal independently and being more restricted
+    # with distance from terminal for including tug traffic in origin-
+    # destination analysis and excluding tug traffic to/from 
+    # lumbar yards (or equivalent) that are often located very close
+    # to oil transfer sites and that were included in Casey's pre-
+    # selected data.  These tracks are then given the following
+    # attribution of origin/destination as generic "Pacific", "US", 
+    # and "Canada" to exclude this traffic as oil cargo traffic
+    # Here, I flag these ship tracks as non-oil traffic by setting
+    # fuel_flag to 1.
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     elif origin == 'Pacific':
         print(origin)
         fuel_flag = 1
-        fuel_type = get_oil_type_fuel(fuel_yaml, ship_type, ship_type, random_seed)
+        fuel_type = []
+#        fuel_type = get_oil_type_fuel(
+#                  fuel_yaml, ship_type, random_seed)
     elif origin == 'US':
         fuel_flag = 1
-        fuel_type = get_oil_type_fuel(fuel_yaml, ship_type, ship_type, random_seed)
+        fuel_type = []
+#         fuel_type = get_oil_type_fuel(
+#                   fuel_yaml, ship_type, random_seed)
     elif origin == 'Canada':
         fuel_flag = 1
-        fuel_type = get_oil_type_fuel(fuel_yaml, ship_type, ship_type, random_seed)
+        fuel_type = []
+#         fuel_type = get_oil_type_fuel(
+#                  fuel_yaml, ship_type, random_seed)
     else:
         fuel_flag = 1
-        fuel_type = get_oil_type_fuel(fuel_yaml, ship_type, ship_type, random_seed)
-        
+        fuel_type = []
+#         fuel_type = get_oil_type_fuel(
+#                   fuel_yaml, ship_type, random_seed)
+    
+             
     return fuel_type, fuel_flag
