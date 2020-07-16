4560af054e7ab4e41bc8761654e42dbbdeeaf3ec
Rachael D. Mueller <rmueller@eoas.ubc.ca>
2020-07-16 10:55:19 -0700

    changed fuel_type to oil_type in get_oil_type_tanker

--- notebooks/monte_carlo/get_oil_type_tanker.py
+++ notebooks/monte_carlo/get_oil_type_tanker.py
@@ -2,6 +2,8 @@ import numpy
 import yaml
 import pathlib
 from monte_carlo_utils import get_oil_type_cargo 
+from monte_carlo_utils import get_oil_type_cargo_generic_US
+
 
 #### Decision tree for allocating oil type to tanker traffic
 # see google drawing [Tanker_Oil_Attribution](https://docs.google.com/drawings/d/1-4gl2yNNWxqXK-IOr4KNZxO-awBC-bNrjRNrt86fykU/edit) for a visual representation
@@ -22,8 +24,10 @@ def get_oil_type_tanker(master_dir,
         master = yaml.safe_load(file)
 
     # Assign US and CAD origin/destinations from master file
-    CAD_origin_destination = master['categories']['CAD_origin_destination']
-    US_origin_destination = master['categories']['US_origin_destination']
+    CAD_origin_destination = master['categories']\
+        ['CAD_origin_destination']
+    US_origin_destination = master['categories']\
+        ['US_origin_destination']
 
     # Get file paths to fuel-type yaml files
     # US_origin is for US as origin
@@ -38,33 +42,62 @@ def get_oil_type_tanker(master_dir,
 
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # NOTE: these pairs need to be used together for "get_oil_type_cargo" (but don't yet have error-checks in place):
+    # NOTE: these pairs need to be used together for 
+    # "get_oil_type_cargo" (but don't yet have error-checks in place):
     # - "WA_in_yaml" and "destination"
     # - "WA_out_yaml" and "origin"
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     if origin in CAD_origin_destination:
         if origin == 'Westridge Marine Terminal':
-            fuel_type = get_oil_type_cargo(CAD_yaml, origin, ship_type, random_seed)
+            oil_type = get_oil_type_cargo(
+                CAD_yaml, origin, 
+                ship_type, random_seed
+            )
         else:
             if destination in US_origin_destination:
-                # we have better information on WA fuel transfers, so I'm prioritizing this information source
-                fuel_type = get_oil_type_cargo(WA_in_yaml, destination, ship_type, random_seed)
+                # we have better information on WA fuel transfers, 
+                #so I'm prioritizing this information source
+                oil_type = get_oil_type_cargo(
+                    WA_in_yaml, destination, 
+                    ship_type, random_seed
+                )
             else:
-                fuel_type = get_oil_type_cargo(CAD_yaml, origin, ship_type, random_seed)
+                oil_type = get_oil_type_cargo(
+                    CAD_yaml, origin, 
+                    ship_type, random_seed
+                )
     elif origin in US_origin_destination:  
-        fuel_type = get_oil_type_cargo(WA_out_yaml, origin, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            WA_out_yaml, origin, 
+            ship_type, random_seed
+        )
     elif destination in US_origin_destination:
-        fuel_type = get_oil_type_cargo(WA_in_yaml, destination, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            WA_in_yaml, destination, 
+            ship_type, random_seed
+        )
     elif destination in CAD_origin_destination:
-        fuel_type = get_oil_type_cargo(CAD_yaml, destination, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            CAD_yaml, destination, 
+            ship_type, random_seed
+        )
     elif origin == 'Pacific':
-        fuel_type = get_oil_type_cargo(Pacific_yaml, origin, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            Pacific_yaml, origin, 
+            ship_type, random_seed
+        )
     elif origin == 'US':
-        fuel_type = get_oil_type_cargo(US_yaml, origin, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            US_yaml, origin, 
+            ship_type, random_seed
+        )
     else: 
         # Currently, this is a catch for all ship tracks not allocated with origin or destination
         # It's a generic fuel attribution from the combined US import and export
-        fuel_type = get_oil_type_cargo(USall_yaml, origin, ship_type, random_seed)
+        oil_type = get_oil_type_cargo(
+            USall_yaml, origin, 
+            ship_type, random_seed
+        )
         
-    return fuel_type
+    return oil_type
