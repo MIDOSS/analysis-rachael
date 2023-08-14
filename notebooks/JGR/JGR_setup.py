e616c252c12cf70b7a04247ede333648f19104d5
Rachael D. Mueller <rmueller@eoas.ubc.ca>
2023-08-14 10:35:38 -0700

    Added JGR graphic notebook for oil capacities, in both US and CAD units

---
+++ notebooks/JGR/JGR_setup.py
@@ -0,0 +1,74 @@
+# oil attributions 
+vessels = [
+    'atb', 'barge','tanker','cargo','cruise',
+    'ferry','fishing','other','smallpass'
+]
+
+vesselcolors={
+    'atb': 'olive',
+    'barge':"darkolivegreen",
+    'tanker':'darkslategrey',
+    'cargo':"darkgoldenrod",
+    'cruise':"darkkhaki",
+    'ferry':"cadetblue",
+    'fishing':"steelblue",
+    'smallpass':"orchid",
+    'other':"goldenrod"
+}
+
+# *** types, labels, and fnames need to be ordered in the same way ***
+types = [
+    'akns', 
+    'bunker', 
+    'dilbit', 
+    'diesel',  
+    'gas',
+    'jet', 
+    'other'
+]
+labels = [
+    'ANS', 
+    'Bunker-C', 
+    'Dilbit', 
+    'Diesel', 
+    'Gasoline',
+    'Jet Fuel', 
+    'Other'
+]
+colors = [
+    'darkslategrey',
+    'teal',
+    'slategrey',
+    'cornflowerblue',
+    'cornflowerblue',
+    'cornflowerblue',
+    'teal'
+]
+fnames = [
+    'Lagrangian_akns.dat',
+    'Lagrangian_bunker.dat',
+    'Lagrangian_dilbit.dat',
+    'Lagrangian_diesel.dat',
+    'Lagrangian_gas.dat',
+    'Lagrangian_jet.dat',
+    'Lagrangian_other.dat'
+]
+MIDOSSlabels=[
+    'ANS',
+    'Bunker-C',
+    'Dilbit',
+    'Diesel',
+    'Diesel',
+    'Diesel',
+    'Bunker-C'
+]
+MIDOSScolors={
+    'ANS': 'darkslategrey',
+    'Bunker-C':'teal',
+    'Dilbit':'slategrey',
+    'Diesel':'cornflowerblue',
+}
+
+
+
+ 
